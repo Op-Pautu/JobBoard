0cb5f15617ffc498a5b28f51eaca5e704a737b8a
Op-Pautu <pautuop@gmail.com>
2023-07-21 19:19:52 +0530

    Implement search filter

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,6 +2,7 @@
   <x-breadcrumbs class="mb-4"
     :links="['Jobs' => route('jobs.index')]" />
     <x-card class="mb-4 text-sm">
+      <form action="{{ route('jobs.index') }}" method="GET">
       <div class="mb-4 grid grid-cols-2 gap-4">
         <div>
           <div class="mb-1 font-semibold">Search</div>
@@ -18,6 +19,8 @@
         <div>3</div>
         <div>4</div>
       </div>
+      <button class="w-full">Filter</button>
+    </form>
     </x-card>
     @foreach ($jobs as $job)
        <x-job-card class="mb-4" :job="$job">
