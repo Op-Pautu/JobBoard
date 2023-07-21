b43974b821f0d016891a10c6d03e64905562c928
Op-Pautu <pautuop@gmail.com>
2023-07-21 23:05:43 +0530

    Fix labels not syncing with radio

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -1,13 +1,13 @@
 <div>
-    <label for="{{ $name }}" class="mb-1 flex items-center">
+    <label for="{{ $name }}_all" class="mb-1 flex items-center">
       <input type="radio" name="{{ $name }}" value=""
-        @checked(!request($name)) />
+        @checked(!request($name)) id="{{ $name }}_all"/>
       <span class="ml-2">All</span>
     </label>
   
     @foreach ($optionsWithLabels as $label => $option)
-      <label for="{{ $name }}" class="mb-1 flex items-center">
-        <input type="radio" name="{{ $name }}" value="{{ $option }}"
+      <label for="{{ $name }}_{{ $option }}" class="mb-1 flex items-center">
+        <input type="radio" name="{{ $name }}" id="{{ $name }}_{{ $option }}" value="{{ $option }}"
           @checked($option === request($name)) />
         <span class="ml-2">{{ $label }}</span>
       </label>
