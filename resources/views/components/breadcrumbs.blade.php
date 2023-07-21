4233eb7df305e12d975a210ed8f5a54e86264e47
Op-Pautu <pautuop@gmail.com>
2023-07-21 13:57:58 +0530

    Add breadcrumbs nav

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,3 +1,13 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-</div>
+<nav {{ $attributes }} >
+    <ul class="flex space-x-4 text-slate-500">
+        <li>
+            <a href="/">Home</a>
+        </li>
+        @foreach ($links as $label => $link)
+            <li>→</li>
+            <li>
+                <a href="{{ $link }}">{{ $label }} </a>
+            </li>
+        @endforeach
+    </ul>
+</nav>
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,18 +1,6 @@
 <x-layout>
-    <nav class="mb-4">
-        <ul class="flex space-x-4 text-slate-500">
-            <li>
-                <a href="/">Home</a>
-            </li>
-            <li>→</li>
-            <li>
-                <a href="{{ route('jobs.index')}}">Jobs</a>
-            </li>
-            <li>→</li>
-            <li>
-                {{ $job->title }}
-            </li>
-        </ul>
-    </nav>
+    <x-breadcrumbs class="mb-4" 
+        :links="['Jobs' => route('jobs.index'), $job->title => '#']"
+    />
     <x-job-card :$job/>
 </x-layout>
