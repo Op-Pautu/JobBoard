87b31797247dfb8e8d253d71a97b11cccc609296
Op-Pautu <pautuop@gmail.com>
2023-07-26 15:08:05 +0530

    Refactor bg, style btn and add alpinejs

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,31 +2,34 @@
   <x-breadcrumbs class="mb-4"
     :links="['Jobs' => route('jobs.index')]" />
     <x-card class="mb-4 text-sm">
-      <form action="{{ route('jobs.index') }}" method="GET">
-      <div class="mb-4 grid grid-cols-2 gap-4">
-        <div>
-          <div class="mb-1 font-semibold">Search</div>
-          <x-text-input name="search" value="{{  request('search') }}" placeholder="Search for any text"/>
-        </div>
-        <div>
-          <div class="mb-1 font-semibold">Salary</div>
-          <div class="flex space-x-2">
-            <x-text-input name="min_salary" value="{{ request('min_salary')}}" placeholder="From"/>
-            <x-text-input name="max_salary" value="{{ request('max_salary')}}" placeholder="To"/>
+      <form id="filtering-form" action="{{ route('jobs.index') }}" method="GET">
+        <div class="mb-4 grid grid-cols-2 gap-4">
+          <div>
+            <div class="mb-1 font-semibold">Search</div>
+            <x-text-input name="search" value="{{  request('search') }}" 
+              placeholder="Search for any text"
+              formId="filtering-form"
+              />
           </div>
-        
-        </div>
-        <div>
-          <div class="mb-1 font-semibold">Experience</div>
-            <x-radio-group name="experience" :options="array_combine(array_map('ucfirst' ,\App\Models\Job::$experience) ,\App\Models\Job::$experience)"/>
+          <div>
+            <div class="mb-1 font-semibold">Salary</div>
+            <div class="flex space-x-2">
+              <x-text-input name="min_salary" value="{{ request('min_salary')}}" placeholder="From" formId="filtering-form"/>
+              <x-text-input name="max_salary" value="{{ request('max_salary')}}" placeholder="To" formId="filtering-form"/>
+            </div>
+          
           </div>
-        <div>
-          <div class="mb-1 font-semibold">Category</div>
-          <x-radio-group name="category" :options="\App\Models\Job::$category"/>
+          <div>
+            <div class="mb-1 font-semibold">Experience</div>
+              <x-radio-group name="experience" :options="array_combine(array_map('ucfirst' ,\App\Models\Job::$experience) ,\App\Models\Job::$experience)"/>
+            </div>
+          <div>
+            <div class="mb-1 font-semibold">Category</div>
+            <x-radio-group name="category" :options="\App\Models\Job::$category"/>
+          </div>
+          </div>
         </div>
-        </div>
-      </div>
-      <button class="w-full">Filter</button>
+        <x-button class="w-full">Filter</x-button>
     </form>
     </x-card>
     @foreach ($jobs as $job)
