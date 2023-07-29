ba1035b31d6f091e08812cdc7dbf14d17193e10e
Op-Pautu <pautuop@gmail.com>
2023-07-29 13:27:06 +0530

    Add controller and view for my_applications

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\MyJobApplicationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,3 +36,6 @@ Route::middleware('auth')->group(function() {
     Route::resource('job.application', JobApplicationController::class)
     ->only(['create', 'store']);
 });
+
+Route::resource('my-job-applications', MyJobApplicationController::class)
+    ->only(['index', 'destroy']);
