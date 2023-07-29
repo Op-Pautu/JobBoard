5aad76ef76b9e23641df54ca9ccb7792fb4234de
Op-Pautu <pautuop@gmail.com>
2023-07-29 16:58:40 +0530

    Refactor and display errors

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,5 +1,6 @@
 <div class="relative">
-    @if($formRef)
+    @if ('textarea' !== $type)
+        @if($formRef)
         <button 
             type="button"
             class="absolute top-0 right-0 flex h-full items-center pr-2"
@@ -9,11 +10,30 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>  
         </button>
+        @endif
+        <input x-ref="input-{{ $name }} " type={{ $type }} placeholder="{{ $placeholder }}"
+            name=" {{ $name }}" value="{{ old($name, $value) }}" id="{{ $name }}"
+            @class(['w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+            'pr-8' => $formRef,
+            'ring-slate-300' => !$errors->has($name),
+            'ring-red-300' => $errors->has($name),
+            ])
+        />
+    @else 
+        <textarea id="{{ $name }}  name=" {{ $name }}"
+        @class(['w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+        'pr-8' => $formRef,
+        'ring-slate-300' => !$errors->has($name),
+        'ring-red-300' => $errors->has($name),
+        ])>
+            {{ old($name, $value) }}
+        </textarea>
     @endif
-    <input x-ref="input-{{ $name }} " type={{ $type }} placeholder="{{ $placeholder }}"
-        name=" {{ $name }}" value="{{ $value }}" id="{{ $name }}"
-        class="w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2 pr-8"
-    />
+    @error($name)
+    <div class="mt-1 text-red-500 text-xs">
+        {{ $message }}
+    </div>
+    @enderror
 </div>
 {{-- onclick="document.getElementById('{{ $name }}').value = '';
 document.getElementById('{{ $formId}}').submit();
