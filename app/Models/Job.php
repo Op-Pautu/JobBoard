87482fc8f57007e73f969a2cf604c0304fa1d4ab
Op-Pautu <pautuop@gmail.com>
2023-07-31 23:54:14 +0530

    Add soft delete

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Job extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'title',
